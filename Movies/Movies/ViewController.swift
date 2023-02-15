efbcd8344bbef6b596208d6075da68b003abfbbc
Ricardo Hernández <ricardo.ohg96@gmail.com>
2023-02-15 10:04:55 -0600

    First commit

---
+++ Movies/Movies/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Movies
+//
+//  Created by Ricardo Omar Hernández on 15/02/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
