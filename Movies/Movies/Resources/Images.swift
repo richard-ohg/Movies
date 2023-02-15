a7b410dd494225bd2948dbeb840f4010a8b39689
Ricardo Hernández <ricardo.ohg96@gmail.com>
2023-02-15 15:41:34 -0600

    add assets and Image Struct with names of assets

---
+++ Movies/Movies/Resources/Images.swift
@@ -0,0 +1,11 @@
+//
+//  Images.swift
+//  Movies
+//
+//  Created by Ricardo Omar Hernández on 15/02/23.
+//
+
+struct Images {
+    static let loginBackgroundImage = "wallpaper"
+    static let loginIcon = "iconMovieDB"
+}
