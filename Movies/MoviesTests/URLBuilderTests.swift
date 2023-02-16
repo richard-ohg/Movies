0d96c46ebed60f5dbebad45758afcb934420c959
Ricardo Hernández <ricardo.ohg96@gmail.com>
2023-02-15 21:06:25 -0600

    test url builder

--- Movies/MoviesTests/URLBuilderTests.swift
+++ Movies/MoviesTests/URLBuilderTests.swift
@@ -1,5 +1,5 @@
 //
-//  MoviesTests.swift
+//  URLBuilderTests.swift
 //  MoviesTests
 //
 //  Created by Ricardo Omar Hernández on 15/02/23.
@@ -8,35 +8,44 @@
 import XCTest
 @testable import Movies
 
-final class MoviesTests: XCTestCase {
+final class URLBuilderTests: XCTestCase {
 
     func testBuildValidBaseURL() {
         let builder = URLBuilder()
         XCTAssertEqual(builder.scheme, "https")
         XCTAssertEqual(builder.method, Method.GET)
-        XCTAssertEqual(builder.getBaseUrl()!, URL(string: "https://api.themoviedb.org/3")!)
+        XCTAssertEqual(builder.getUrl(), URL(string: "https://api.themoviedb.org/3"))
     }
     
     func testBuildInvalidURL() {
         var builder = URLBuilder(baseUrl: "example.com")
         builder.config(path: .custom("//@3`-"))
-        XCTAssertNil(builder.getBaseUrl())
+        XCTAssertNil(builder.getUrl())
     }
     
     func testBuildForceSchemeURL() {
         let builder = URLBuilder(baseUrl: "http://example.com/")
-        XCTAssertEqual(builder.getBaseUrl(), URL(string: "https://example.com/3")!)
+        XCTAssertEqual(builder.getUrl(), URL(string: "https://example.com/3"))
     }
 
     func testBuildOverrideSchemeURL() {
         var builder = URLBuilder(baseUrl: "http://example.com/")
         builder.config(scheme: "ftp")
-        XCTAssertEqual(builder.getBaseUrl(), URL(string: "ftp://example.com/3")!)
+        XCTAssertEqual(builder.getUrl(), URL(string: "ftp://example.com/3"))
     }
     
     func testBuildWithQueryParams() {
         var builder = URLBuilder()
         builder.config(path: .popularMovies(), queryParams: ["id" : 3])
-        XCTAssertEqual(builder.getUrl()!, URL(string: "https://api.themoviedb.org/3/movie/popular?api_key=af1d327389d620e0983e23be3562cc8d&id=3")!)
+        XCTAssertEqual(builder.getUrl(), URL(string: "https://api.themoviedb.org/3/movie/popular?id=3"))
+    }
+    
+    func testBuildPostUrlRequest() {
+        var builder = URLBuilder()
+        builder.config(path: .popularMovies())
+        builder.method = .POST
+        let url = builder.getUrlRequest()
+        XCTAssertEqual(builder.method, Method.POST)
+        XCTAssertEqual(builder.getUrl(), URL(string: "https://api.themoviedb.org/3/movie/popular"))
     }
 }
