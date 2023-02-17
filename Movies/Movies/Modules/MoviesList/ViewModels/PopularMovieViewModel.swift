34f81498b31e937c1e8a6ae525a5596f28ef713e
Ricardo Hernández <ricardo.ohg96@gmail.com>
2023-02-16 21:57:35 -0600

    ViewModel to map ResponseEntity, Mapper implemented

---
+++ Movies/Movies/Modules/MoviesList/ViewModels/PopularMovieViewModel.swift
@@ -0,0 +1,16 @@
+//
+//  PopularMovieViewModel.swift
+//  Movies
+//
+//  Created by Ricardo Omar Hernández on 16/02/23.
+//
+
+import Foundation
+
+struct PopularMovieViewModel {
+    let imageUrl: URL?
+    let title: String
+    let date: String
+    let score: String
+    let description: String
+}
